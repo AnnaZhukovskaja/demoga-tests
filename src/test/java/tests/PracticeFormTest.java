b0e8b844e1580bcc33558d5d8c58ea602364c9b0
Anna Zhukovskaya <kulnis.anna@gmail.com>
2024-06-17 20:56:19 +0300

    added PracticeFormTest

---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,78 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000;
+    }
+
+    @Test
+    void fillFormTest() {
+
+        open("/automation-practice-form");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Semenov");
+        $("#userEmail").setValue("qa@quru.com");
+
+        $("#genterWrapper").$(byText("Male")).click();
+
+        $("#userNumber").setValue("3752955556");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1999");
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__day--002:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjects-label").scrollTo();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+
+        $("#subjects-label").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+
+        File file = new File("src/test/resources/Hello.docx");
+       $("#uploadPicture").uploadFile(file);
+
+        $("#currentAddress").setValue("Minsk");
+
+        $("#submit").scrollTo();
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Agra")).click();
+
+       $("#submit").click();
+
+       $("#example-modal-sizes-title-lg ").shouldHave(exactText("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldHave(
+                Condition.text("Student Name Ivan Semenov"),
+                Condition.text("Student Email qa@quru.com"),
+                Condition.text("Gender Male"),
+                Condition.text("Mobile 3752955556"),
+                Condition.text("Date of Birth 02 February,1999"),
+                Condition.text("Subjects Maths"),
+                Condition.text("Hobbies Sports"),
+                Condition.text("Picture Hello.docx"),
+                Condition.text("Address Minsk"),
+                Condition.text("State and City Uttar Pradesh Agra")
+        );
+    }
+}
