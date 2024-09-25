f5a268b0abaeac465b1253059020ef34d0682998
Anna Zhukovskaya <kulnis.anna@gmail.com>
2024-09-25 17:40:12 +0300

    added allure and attach in PracticeFormWithPageObjectTest.java

--- src/test/java/tests/PracticeFormWithPageObjectTest.java
+++ src/test/java/tests/PracticeFormWithPageObjectTest.java
@@ -1,8 +1,11 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import utils.TestData;
+
+import static io.qameta.allure.Allure.step;
 
 
 public class PracticeFormWithPageObjectTest extends BaseTest{
@@ -11,69 +14,81 @@ public class PracticeFormWithPageObjectTest extends BaseTest{
     TestData testData = new TestData();
 
 
+    @Tag("demoqa HW")
     @Test
     void successfulRegistrationTest() {
-
-        registrationPage.
-                openPage().
-                setFirstName(testData.firstName).
-                setLastName(testData.lastName).
-                setUserEmail(testData.emailAddress).
-                setGenderWrapper(testData.gender).
-                setUserNumber(testData.mobileNumber).
-                setDateOfBirth(testData.monthOfBirth,testData.yearOfBirth,testData.dayOfBirth).
-                setSubjects(testData.subjects).
-                setHobbiesWrapper(testData.hobbies).
-                setUploadPicture(testData.image).
-                setCurrentAddress(testData.currentAddress).
-                setState(testData.state).
-                setCity(testData.city).
-                clickSubmit();
-
-        registrationPage.checkTitleInModalForm("Thanks for submitting the form");
-        registrationPage.checkDataInTable("Student Name",testData.firstName + " " + testData.lastName).
-                checkDataInTable("Student Email",testData.emailAddress).
-                checkDataInTable("Gender",testData.gender).
-                checkDataInTable("Mobile",testData.mobileNumber).
-                checkDataInTable("Date of Birth",testData.dayOfBirth + " " + testData.monthOfBirth + "," + testData.yearOfBirth).
-                checkDataInTable("Subjects",testData.subjects).
-                checkDataInTable("Hobbies",testData.hobbies).
-                checkDataInTable("Picture",testData.image).
-                checkDataInTable("Address", testData.currentAddress).
-                checkDataInTable("State and City",testData.state +" " + testData.city);
+        step("Open form and full", () -> {
+                    registrationPage.
+                            openPage().
+                            setFirstName(testData.firstName).
+                            setLastName(testData.lastName).
+                            setUserEmail(testData.emailAddress).
+                            setGenderWrapper(testData.gender).
+                            setUserNumber(testData.mobileNumber).
+                            setDateOfBirth(testData.monthOfBirth, testData.yearOfBirth, testData.dayOfBirth).
+                            setSubjects(testData.subjects).
+                            setHobbiesWrapper(testData.hobbies).
+                            setUploadPicture(testData.image).
+                            setCurrentAddress(testData.currentAddress).
+                            setState(testData.state).
+                            setCity(testData.city).
+                            clickSubmit();
+                });
+        step("Verify results", () -> {
+            registrationPage.checkTitleInModalForm("Thanks for submitting the form");
+            registrationPage.checkDataInTable("Student Name", testData.firstName + " " + testData.lastName).
+                    checkDataInTable("Student Email", testData.emailAddress).
+                    checkDataInTable("Gender", testData.gender).
+                    checkDataInTable("Mobile", testData.mobileNumber).
+                    checkDataInTable("Date of Birth", testData.dayOfBirth + " " + testData.monthOfBirth + "," + testData.yearOfBirth).
+                    checkDataInTable("Subjects", testData.subjects).
+                    checkDataInTable("Hobbies", testData.hobbies).
+                    checkDataInTable("Picture", testData.image).
+                    checkDataInTable("Address", testData.currentAddress).
+                    checkDataInTable("State and City", testData.state + " " + testData.city);
+        });
     }
 
+    @Tag("demoqa HW")
     @Test
     void successfulRegistrationWithMinimumDataTest() {
 
-        registrationPage.
-                openPage().
-                setFirstName(testData.firstName).
-                setLastName(testData.lastName).
-                setGenderWrapper(testData.gender).
-                setUserNumber(testData.mobileNumber).
-                clickSubmit();
+        step("Open form and full", () -> {
+                    registrationPage.
+                            openPage().
+                            setFirstName(testData.firstName).
+                            setLastName(testData.lastName).
+                            setGenderWrapper(testData.gender).
+                            setUserNumber(testData.mobileNumber).
+                            clickSubmit();
+                });
 
-        registrationPage.checkTitleInModalForm("Thanks for submitting the form");
-        registrationPage.checkDataInTable("Student Name",testData.firstName + " " + testData.lastName).
-                checkDataInTable("Student Email"," ").
-                checkDataInTable("Gender",testData.gender).
-                checkDataInTable("Mobile",testData.mobileNumber).
-                checkDataInTable("Date of Birth",testData.currentDay).
-                checkDataInTable("Subjects"," ").
-                checkDataInTable("Hobbies"," ").
-                checkDataInTable("Picture"," ").
-                checkDataInTable("Address", " ").
-                checkDataInTable("State and City"," ");
+        step("Verify results", () -> {
+            registrationPage.checkTitleInModalForm("Thanks for submitting the form");
+            registrationPage.checkDataInTable("Student Name", testData.firstName + " " + testData.lastName).
+                    checkDataInTable("Student Email", " ").
+                    checkDataInTable("Gender", testData.gender).
+                    checkDataInTable("Mobile", testData.mobileNumber).
+                    checkDataInTable("Date of Birth", testData.currentDay).
+                    checkDataInTable("Subjects", " ").
+                    checkDataInTable("Hobbies", " ").
+                    checkDataInTable("Picture", " ").
+                    checkDataInTable("Address", " ").
+                    checkDataInTable("State and City", " ");
+        });
     }
+    @Tag("demoqa HW")
     @Test
     void negativeRegistrationTest() {
 
-        registrationPage.
-                openPage().
-                setFirstName(testData.firstName).
-                clickSubmit();
-
-        registrationPage.checkAppearanceOfModalForm();
+        step("Open form and full", () -> {
+                    registrationPage.
+                            openPage().
+                            setFirstName(testData.firstName).
+                            clickSubmit();
+                });
+        step("Verify results", () -> {
+            registrationPage.checkAppearanceOfModalForm();
+        });
     }
 }
